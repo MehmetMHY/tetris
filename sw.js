dc69787d636ede2a53b33560c2f89e36c7e122ac
mehmet-mhy <mehmet.mhy@gmail.com>
2026-09-11 22:03:45 -0600

    added report/issue svg button to the front page that takes the users to the project's github issues page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tetris-v52";
+const CACHE_NAME = "tetris-v53";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,7 @@ const ASSETS = [
   "./assets/logo.png",
   "./assets/github.svg",
   "./assets/settings.svg",
+  "./assets/report.svg",
   "./assets/apple-touch-icon.png",
   "./assets/favicon-32x32.png",
   "./assets/favicon-16x16.png",
